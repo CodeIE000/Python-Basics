844bf15be9d1911b7dd696b00b8195c095e50e4b
Code <idulsa.emmannicholasblabe.000@gmail.com>
2024-01-08 09:01:08 +0800

    sixth commit

---
+++ Intermediate/comprehensions.py
@@ -0,0 +1,19 @@
+#Comprehensions
+
+x = [x for x in range(5)]
+print(x)
+x = [x + 4 for x in range(5)]
+print(x)
+x = [x % 4 for x in range(5)]
+print(x)
+x = [0 for x in range(5)]
+print(x)
+
+x = [[0 for x in range(20)] for x in range(5)]
+print(x)
+
+x = [i for i in range(20) if i % 5 == 0]
+print(x)
+
+x = {i: 0 for i in range(20) if i % 5 == 0}
+print(x)
